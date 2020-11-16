41bd026e86c2f41105fee9ccb691d82ba0387838
Roberto Aguinaga <raguinaga1@outlook.com>
2020-11-16 00:12:18 -0600

    Cleared away the default page, added some comments. Added the default welcome page

--- HoldingPage/Startup.cs
+++ HoldingPage/Startup.cs
@@ -1,53 +1,14 @@
+/* The startup class handles some infrastructure of the webpage, mainly services
+ and the middleware pipeline. Services are registered here and the middleware
+tells the app how to respond to requests.*/
 namespace HoldingPage
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
-
     using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
-
-    public class Startup
+    // Start up handles service registration and middleware and endpoints
+    public class Startup 
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
-
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
-        }
+        // Configure methods actually defines the middleware pipeline
+        // app welcome page is the only actual middleware in the pipeline
+        public void Configure(IApplicationBuilder app) => app.UseWelcomePage();
     }
 }
