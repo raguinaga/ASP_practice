e1361f57cadf3d744ce0954ab33ffae2839dd5ba
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-12 00:19:39 -0500

    Removed unecessary imports

--- chapter_3/HoldingPage/Startup.cs
+++ chapter_3/HoldingPage/Startup.cs
@@ -1,20 +1,10 @@
 namespace HoldingPage
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
-
     using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.HttpsPolicy;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
 
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app) => _ = app.UseWelcomePage();
     }
 }
