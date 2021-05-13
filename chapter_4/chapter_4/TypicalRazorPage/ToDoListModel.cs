d5d6f4912ee019dee940b57cc0cce25f62a5ef64
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-12 22:12:36 -0500

    Added a simple class that holds a title and model fields

---
+++ chapter_4/chapter_4/TypicalRazorPage/ToDoListModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TypicalRazorPage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class ToDoListModel
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+    }
+}
