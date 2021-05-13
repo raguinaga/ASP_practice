dd2208fa017a16581307c5ae6c8c65e5b92fc433
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-12 21:59:18 -0500

    Added project to highlight razor pages

---
+++ chapter_4/chapter_4/TypicalRazorPage/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿namespace TypicalRazorPage.Pages
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
