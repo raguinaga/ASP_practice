84f62614916c144f11b55a469f7fe8606b23a60f
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-13 18:07:16 -0500

    Simplifed Configure method

--- chapter_4/MvcToDoService/Startup.cs
+++ chapter_4/MvcToDoService/Startup.cs
@@ -7,11 +7,6 @@ namespace MvcToDoService
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
 
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
-
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -24,7 +19,8 @@ namespace MvcToDoService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            _ = services.AddControllersWithViews();
+            _ = services.AddSingleton<ToDoService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,25 +28,20 @@ namespace MvcToDoService
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                _ = app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                _ = app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
-            app.UseRouting();
+            _ = app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+            _ = app.UseEndpoints(endpoints =>
+              {
+                  _ = endpoints.MapDefaultControllerRoute();
+              });
         }
     }
 }
