560d1fee66f15b6fd4a2763d67f8c23e57f53193
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-13 18:07:31 -0500

    Added domain modeling classes for a to-do service

---
+++ chapter_4/MvcToDoService/ToDoListModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcToDoService
+{
+
+    public class ToDoListModel
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ chapter_4/MvcToDoService/ToDoService.cs
@@ -0,0 +1,22 @@
+﻿namespace MvcToDoService
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ToDoService
+    {
+        public static readonly List<ToDoListModel> items = new()
+        {
+            new ToDoListModel{Category = "Simple", Title = "Bread"},
+            new ToDoListModel{Category = "Simple", Title = "Milk"},
+            new ToDoListModel{Category = "Simple", Title = "Get Gas"},
+            new ToDoListModel{Category = "Long", Title = "Write Book"},
+            new ToDoListModel{Category = "Long", Title = "Build Application"}
+        };
+
+        public List<ToDoListModel> GetItemsForCategory(string category)
+        {
+            return items.Where(x => x.Category == category).ToList();
+        }
+    }
+}
