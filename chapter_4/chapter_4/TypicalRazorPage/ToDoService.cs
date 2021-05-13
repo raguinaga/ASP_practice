362abf215a79f19219105fc5525e520af18c1e38
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-12 22:18:35 -0500

    Made a list of models and a function to return categories as a list

---
+++ chapter_4/chapter_4/TypicalRazorPage/ToDoService.cs
@@ -0,0 +1,24 @@
+﻿namespace TypicalRazorPage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class ToDoService
+    {
+        private static readonly List<ToDoListModel> items = new List<ToDoListModel>
+        {
+            new ToDoListModel{ Category = "Simple", Title = "Bread" },
+            new ToDoListModel{ Category = "Simple", Title = "Milk" },
+            new ToDoListModel{ Category = "Simple", Title = "Get Gas" },
+            new ToDoListModel{ Category = "Long", Title = "Write Book" },
+            new ToDoListModel{ Category = "Long", Title = "Build Application" },
+        };
+
+        public List<ToDoListModel> GetItemsForCategory(string category)
+        {
+            return items.Where(x => x.Category == category).ToList();
+        }
+    }
+}
