f6719888d479be56e7c268ec6968b3cf54891bea
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-13 18:58:48 -0500

    Added a model to MVC app

---
+++ chapter_4/MvcToDoService/Models/CategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MvcToDoService.Models
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class CategoryViewModel
+    {
+        public List<ToDoListModel> Items{ get; set; }
+
+        public CategoryViewModel(List<ToDoListModel> items) => this.Items = items;
+    }
+}
