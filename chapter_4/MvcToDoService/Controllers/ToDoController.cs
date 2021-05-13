c9f26f877edfab1da10d61d2c08d823a671585b4
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-13 18:59:13 -0500

    Added a controller that uses the view model

---
+++ chapter_4/MvcToDoService/Controllers/ToDoController.cs
@@ -0,0 +1,22 @@
+﻿namespace MvcToDoService.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using MvcToDoService.Models;
+    using System.Collections.Generic;
+
+    public class ToDoController : Controller
+    {
+        private readonly ToDoService service;
+
+        public ToDoController(ToDoService service) => this.service = service;
+
+        public ActionResult Category(string id)
+        {
+            List<ToDoListModel> items = this.service.GetItemsForCategory(id);
+            var viewModel = new CategoryViewModel(items);
+
+            return View(viewModel);
+        }
+    }
+}
