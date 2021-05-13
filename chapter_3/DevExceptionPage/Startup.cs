d8cf5043de8ca095e542962ff55418bb44ab6f89
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-12 21:44:44 -0500

    Refactored startup class to always use dev exception page

--- chapter_3/DevExceptionPage/Startup.cs
+++ chapter_3/DevExceptionPage/Startup.cs
@@ -14,43 +14,17 @@ namespace DevExceptionPage
 
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public void ConfigureServices(IServiceCollection services) => _ = services.AddRazorPages();
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            _ = app.UseDeveloperExceptionPage();
+            _ = app.UseStaticFiles();
+            _ = app.UseRouting();
+
+            _ = app.UseEndpoints(endpoints =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
+                _ = endpoints.MapRazorPages();
             });
         }
     }
