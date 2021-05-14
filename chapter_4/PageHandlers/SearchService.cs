4b30679e0cc30aebe48798c18fe1873c48d766a0
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-14 00:14:37 -0500

    Added class that allows searching of products

---
+++ chapter_4/PageHandlers/SearchService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PageHandlers
+{
+    public class SearchService
+    {
+        public static readonly List<Product> items = new()
+        {
+            new Product {Name = "iPad"},
+            new Product {Name = "iPod"},
+            new Product {Name = "iMac"},
+            new Product {Name = "Mac Pro"},
+            new Product {Name = "Mac Mini"},
+        };
+
+        public List<Product> SearchProducts(string term)
+        {
+            // filter by provided category
+            return items.Where(x => x.Name.Contains(term)).ToList();
+        }
+    }
+}
