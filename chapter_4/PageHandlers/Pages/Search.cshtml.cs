4cc1328bdcadef78ba26c4064cd595904cb941f7
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-14 00:34:59 -0500

    Added code to bind information to post information

---
+++ chapter_4/PageHandlers/Pages/Search.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PageHandlers.Pages
+{
+    public class Search : PageModel
+    {
+        private readonly SearchService searchService;
+        public Search(SearchService service) => this.searchService = service;
+        
+        [BindProperty]
+        public BindingModel Input { get; set; }
+        public List<Product> Results { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToPage();
+            }
+            Results = this.searchService.SearchProducts(Input.SearchTerm);
+            return Page();
+        }
+        
+        public class BindingModel
+        {
+            [Required]
+            public string SearchTerm { get; set; }
+        }
+    }
+}
