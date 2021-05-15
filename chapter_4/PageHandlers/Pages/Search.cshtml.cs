30e1740add81c8c41f4aee4791b4337f0baf128b
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-14 20:32:26 -0500

    Added comments

--- chapter_4/PageHandlers/Pages/Search.cshtml.cs
+++ chapter_4/PageHandlers/Pages/Search.cshtml.cs
@@ -7,19 +7,27 @@ namespace PageHandlers.Pages
 {
     public class Search : PageModel
     {
+        /* Sear service is provided to SearchModel for use in page handlers */ 
         private readonly SearchService searchService;
         public Search(SearchService service) => this.searchService = service;
         
+        /* Any properties decorated with BindProperty attribute will be model
+         * bound */
         [BindProperty]
         public BindingModel Input { get; set; }
+        
+        // Undecorated props will not be model-bound
         public List<Product> Results { get; set; }
-
+        
+        // Page handler does not need to check if the model is valid
         public void OnGet()
         {
         }
 
         public IActionResult OnPost()
         {
+            // All bound properties should be checked for validity using
+            // ModelState.IsValid 
             if (!ModelState.IsValid)
             {
                 return RedirectToPage();
