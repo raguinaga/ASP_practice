1220bc113def0ad90d4d1959d15215526a5ef068
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-15 17:45:58 -0500

    Added code behind for new razor page

---
+++ chapter_5/Chapter_5/Routing_Examples/Pages/Products.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Routing_Examples.Pages
+{
+    public class Products : PageModel
+    {
+        private readonly ProductService service;
+
+        public Products(ProductService service)
+        {
+            this.service = service;
+        }
+        
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+        public Product Selected { get; set; }
+        
+        public IActionResult OnGet()
+        {
+            Selected = service.GetProduct(Name);
+            if (Selected is null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+    }
+}
