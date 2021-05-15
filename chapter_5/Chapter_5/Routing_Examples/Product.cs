cdb5271acc2a5fe6e1b8f38b4f22d5fd656d32fb
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-14 23:52:59 -0500

    Added class to model a product

---
+++ chapter_5/Chapter_5/Routing_Examples/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Routing_Examples
+{
+    public class Product
+    {
+        public string Name { get; }
+        public decimal Price { get; }
+
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
