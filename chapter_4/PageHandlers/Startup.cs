e04825f4562624e88940bb7a08f48f1f3c0a9f7c
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-14 23:39:24 -0500

    Removed unused imports

--- chapter_4/PageHandlers/Startup.cs
+++ chapter_4/PageHandlers/Startup.cs
@@ -1,15 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ASP;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using PageHandlers.Pages;
 
 namespace PageHandlers
 {
