b7f0794acff5a59f5e458a533a25b3b9cf857409
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-14 23:53:25 -0500

    Added a class to sort and return product lists

---
+++ chapter_5/Chapter_5/Routing_Examples/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Routing_Examples
+{
+    public class ProductService
+    {
+        private static readonly IDictionary<string, Product> allProducts = new Dictionary<string, Product>()
+        {
+            {"big-widget", new Product("Big Widget", 123)},
+            {"super-fancy-widget", new Product("Super Fancy Widget", 456)}
+        };
+
+        public Product GetProduct(string name)
+        {
+            return allProducts.TryGetValue(name, out var product) ? product : null;
+        }
+
+        public List<Product> Search(string term, StringComparison comparisonType)
+        {
+            return allProducts
+                  .Where(x => x.Value.Name.Contains(term, comparisonType))
+                  .Select(x => x.Value)
+                  .ToList();
+        }
+    }
+}
