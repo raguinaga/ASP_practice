4717ef6b5eb1d0a4fedf31f676c00ce45de9af7b
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-18 17:23:06 -0500

    Added new razor page

---
+++ chapter_5/ChangingConventions/Pages/ProductDetails/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChangingConventions.Pages.ProductDetails
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
