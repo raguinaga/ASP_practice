4c199ad1bbad7b16179757aa4b76173a2d52bbd5
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-20 22:49:57 -0500

    Created new record to model a product

---
+++ chapter_6/EditProduct/ProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EditProduct
+{
+    public record ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal SellPrice { get; set; }
+    }
+}
