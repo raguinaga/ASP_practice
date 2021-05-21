4f91c8fd37864c16b0d1bade585725a791cf270d
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-20 22:51:29 -0500

    Added new razor page

---
+++ chapter_6/EditProduct/Pages/EditProduct.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EditProduct.Pages
+{
+    public class EditProduct : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
