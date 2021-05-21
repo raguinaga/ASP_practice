a5e1a47de9304af360b85228cfbf58efe19830ff
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-20 23:19:59 -0500

    Added two properties and action in OnGet handler

--- chapter_6/CalculatorBinding/Pages/CalculateSquare.cshtml.cs
+++ chapter_6/CalculatorBinding/Pages/CalculateSquare.cshtml.cs
@@ -4,9 +4,13 @@ namespace CalculatorBinding.Pages
 {
     public class CalculateSquare : PageModel
     {
-        public void OnGet()
+        /* Note: could use [BindProperty(SupportsGet=true)] here instead of 'value' argument in OnGet */
+        public int Input { get; set; }
+        public int Square { get; set; }
+        public void OnGet(int number)
         {
-            
+            Square = number * number;
+            Input = number;
         }
     }
 }
