add168c2495b02d880d46f0ae4b7a473c0b78559
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-20 22:55:55 -0500

    Added two OnPost page handlers

--- chapter_6/EditProduct/Pages/EditProduct.cshtml.cs
+++ chapter_6/EditProduct/Pages/EditProduct.cshtml.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace EditProduct.Pages
 {
+    [IgnoreAntiforgeryToken]
     public class EditProduct : PageModel
     {
+        private ProductModel Product { get; set; }
         public void OnGet()
         {
             
+        }
+
+        public void OnPost(ProductModel productModel)
+        {
+            Product = productModel;
+        }
+
+        public void OnPostEditTwoProducts(ProductModel p1, ProductModel p2)
+        {
+            Product = p1;
         }
     }
 }
