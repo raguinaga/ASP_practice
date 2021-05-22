07bffb68fcbb4c52f120a6f737df78c921d33d1f
Robert Aguinaga <raguinaga1@outlook.com>
2021-05-21 20:55:55 -0400

    Added new razor page

---
+++ chapter_6/CurrencyConverter/Pages/Convert.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CurrencyConverter.Pages
+{
+    public class Convert : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
