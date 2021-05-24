e31df7641dc3608b73d21504fbb6b81ff1d86cf7
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-24 15:51:39 -0500

    Added a new razor page, plus code-behind files

---
+++ chapter_6/DataValidation/Pages/Checkout.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DataValidation.Pages
+{
+    public class Checkout : PageModel
+    {
+        [BindProperty]
+        public UserBindingModel Input { get; init; }
+        
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("Success");
+        }
+        
+        public record UserBindingModel
+        {
+            [Required]
+            [StringLength(100)]
+            [Display(Name = "Your Name")]
+            public string FirstName { get; init; }
+            
+            [Required]
+            [StringLength(100)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; init; }
+            
+            [Required]
+            [EmailAddress]
+            public string Email { get; init; }
+            
+            [Phone]
+            [Display(Name = "Phone Number")]
+            public string PhoneNumber { get; init; }
+        }
+    }
+}
---
+++ chapter_6/DataValidation/Pages/Success.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DataValidation.Pages
+{
+    public class Success : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
