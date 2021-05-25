77d387e13e13d8ddb4871d3d872f809ff6bc0ba2
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-24 22:08:30 -0500

    Added new razor page and overrode method in Attributes class

---
+++ chapter_6/CurrencyConverterValidated/Pages/Currency/Convert.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CurrencyConverterValidated.Pages.Currency
+{
+    public class Convert : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
