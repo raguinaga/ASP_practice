c4eed5ad2961ed89c23b249cb608f288526742fb
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-24 22:13:25 -0500

    Added binding model class, auto-property and Post handler

--- chapter_6/CurrencyConverterValidated/Pages/Currency/Convert.cshtml.cs
+++ chapter_6/CurrencyConverterValidated/Pages/Currency/Convert.cshtml.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CurrencyConverterValidated.Pages.Currency
 {
     public class Convert : PageModel
     {
+        [BindProperty]
+        public InputModel Model { get; init; }
         public void OnGet()
         {
             
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("Success");
+        }
+        
+        public record InputModel
+        {
+            [Required]
+            [StringLength(3, MinimumLength = 3)]
+            [CurrencyCode("GBP", "USD", "CAD", "EUR")]
+            public string CurrencyFrom { get; init; }
+            
+            [Required]
+            [StringLength(3, MinimumLength = 3)]
+            [CurrencyCode("GBP", "USD","CAD", "EUR")]
+            public string CurrencyTo { get; init; }
+            
+            [Required]
+            [Range(1, 1_000)]
+            public decimal Quantity { get; init; }
         }
     }
 }
