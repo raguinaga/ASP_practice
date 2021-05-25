df6576b461a48de4cfc406074499118c7eb6adde
Roberto Aguinaga <raguinaga1@outlook.com>
2021-05-24 22:21:20 -0500

    Added a simple razor page with success message

---
+++ chapter_6/CurrencyConverterValidated/Pages/Currency/Success.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CurrencyConverterValidated.Pages.Currency
+{
+    public class Success : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
