04a89a6aec048644a5dc9c38b79fe237cb28d0c9
Roberto Aguinaga <raguinaga1@outlook.com>
2021-06-13 00:50:21 -0500

    Added new class to model tasks that need to be done

---
+++ chapter_7/ToDoList/ToDoItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    public class ToDoItem
+    {
+        // fields
+        public int Id { get; init; }
+        public List<string> Tasks { get; init; }
+        public bool IsComplete => Tasks.Count == 0;
+        
+        public ToDoItem(int id, params string[] tasks)
+        {
+            Id = id;
+            Tasks = new List<string>(tasks);
+        }
+    }
+}
