474e0cfaa52aae8fddcc2b3ac60d78f0623a6967
Roberto Aguinaga <raguinaga1@outlook.com>
2021-06-13 00:57:21 -0500

    Added a list that uses to-do items

---
+++ chapter_7/ToDoList/ToDoService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ToDoList
+{
+    public class ToDoService
+    {
+        public List<ToDoItem> AllItems { get; } = new()
+        {
+            new ToDoItem(1, "Buy Milk", "Buy Eggs", "Buy Bread"),
+            new ToDoItem(2, "Pick up kids", "Take kids to school"),
+            new ToDoItem(3, "Get Fuel", "<strong>Check oil</strong>", "Check tire pressure"),
+            new ToDoItem(4, "Get fuel", "check oil", "check tire pressure"),
+        };
+    }
+}
