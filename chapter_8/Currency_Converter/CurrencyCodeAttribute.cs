4ba69ceb8acd318142df72fb314c4fdb12733bab
Roberto Aguinaga <raguinaga1@outlook.com>
2021-08-22 13:31:00 -0500

    Added class that uses data validation for currency codes

---
+++ chapter_8/Currency_Converter/CurrencyCodeAttribute.cs
@@ -0,0 +1,23 @@
+﻿namespace CurrencyConverter
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    public class CurrencyCodeAttribute : ValidationAttribute
+    {
+        private readonly string[] allowedCodes;
+
+        public CurrencyCodeAttribute(params string[] allowedCodes)
+        {
+            this.allowedCodes = allowedCodes;
+        }
+
+        protected override ValidationResult? IsValid(
+            object value,
+            ValidationContext validationContext)
+        {
+            return value is not string code || !allowedCodes.Contains(code)
+                ? new ValidationResult("Not a valid currency code")
+                : ValidationResult.Success;
+        }
+    }
+}
