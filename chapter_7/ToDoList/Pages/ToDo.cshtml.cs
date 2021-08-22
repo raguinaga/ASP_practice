4dca9bac9294fbd7cfa3a5f5bf11252e92fdd963
Roberto Aguinaga <raguinaga1@outlook.com>
2021-08-21 22:37:13 -0500

    Minor editing fixes

---
+++ chapter_7/ToDoList/Pages/ToDo.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ToDoList.Pages
+{
+    public class ToDo : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
