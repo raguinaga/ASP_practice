dd892157507c35fa6b679d459ae59b2578552d7e
Roberto Aguinaga <raguinaga1@outlook.com>
2021-11-10 17:23:29 -0600

    More files for the project

---
+++ chapter_8/Currency_Converter/Currency_Converter/CurrencyCodeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Currency_Converter
+{
+    public class CurrencyCodeAttribute : ValidationAttribute
+    {
+        private readonly string[] allowedCodes;
+
+        public CurrencyCodeAttribute(params string[] allowedCodes)
+        {
+            this.allowedCodes = allowedCodes;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is not string code || !this.allowedCodes.Contains(code))
+            {
+                return new ValidationResult("Not a valid currency code");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
